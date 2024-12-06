9e78f7527895140b9285e49a1eb8b2ac16f90769
Andres Taborda <84872685+AndresTabor@users.noreply.github.com>
2024-12-06 18:17:11 +0000

    add models

--- src/models.py
+++ src/models.py
@@ -1,29 +1,61 @@
+
+import enum
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Enum, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(50), unique=True, nullable=False)
+    favorites = relationship('Favorite', back_populates='user')
+
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+
+    favorite_id = Column(Integer, primary_key=True)  
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)  
+    type = Column(Enum("planet","character"), nullable=False)  
+
+    user = relationship('User', back_populates='favorites')
+
+    planet_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    planet = relationship('Planet', back_populates='favorites')
+
+    
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=True)
+    character = relationship('Character', back_populates='favorites')
+
+class Character(Base):
+    __tablename__ = 'characters'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    height = Column(Integer)
+    skin_color = Column(String(50))
+    species = Column(String(50))
+    homeworld_id = Column(Integer, ForeignKey('planets.id'))
+    homeworld = relationship('Planet', back_populates='characters')
+    favorites = relationship('Favorite', back_populates='planet')
+
+class Planet(Base):
+    __tablename__ = 'planets'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    climate = Column(String(100))
+    population = Column(Integer)
+    terrain = Column(String(100))
+    characters = relationship('Character', back_populates='homeworld')
+    favorites = relationship('Favorite', back_populates='planet')
 
     def to_dict(self):
         return {}
